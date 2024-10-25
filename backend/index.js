b255dc5f1a40d4566dd57afb517fe4e372b31117
yadavrajneesh481 <56969556+yadavrajneesh481@users.noreply.github.com>
2024-10-25 19:49:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,7 @@ const weatherRoutes = require('./routes');
 
 const app = express();
 
-app.use(cors(
-  {
-    origin:['/'],
-    methods:['GET','POST','PUT','DELETE'],
-    credentials:true
-  }
-)); 
+
 
 
 app.use(express.json());  
